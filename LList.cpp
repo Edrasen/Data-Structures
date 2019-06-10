fface2bf97a67a6f1b745d6770362561dfe4e2e6
edrasen <alain42117@hotmail.com>
2019-06-09 20:46:49 -0500

    Adding LList

--- LList.cpp
+++ LList.cpp
@@ -73,13 +73,165 @@ int Rsum(node *p){
     return p == NULL ? 0 : Rsum(p->next) + p->data;
 }
 
+int MaxNmbr(node *pl){
+    int max = -32768; //min_INT
+    while(pl){
+        if(pl->data > max){
+            max = pl->data;
+        }
+        pl = pl->next;
+    }
+    return max;
+}
+
+int RMax(node *pl){
+    int max = -32768;
+    if(pl == NULL){
+        return max;
+    }
+    else{
+        max = RMax(pl->next);
+        return (max < pl->data)? pl->data : max;
+    }
+}
+
+node* search(node * pl, int key)
+{
+    while(pl){
+        if(key == pl->data)
+            return pl;
+        pl = pl->next;
+    }
+    return NULL;
+}
+
+node* rsearch(node *p, int key)
+{
+    if(p == NULL)
+        return NULL;
+    if(key == p->data)
+        return p;
+    rsearch(p->next, key);
+}
+
+node* lsearch(node *&ls, int key)
+{
+    node *aux = ls;
+    node *q;
+    while (aux != NULL)
+    {
+        if(key == aux->data)
+        {
+            q->next = aux->next;
+            aux->next = ls;
+            ls = aux;
+            return aux;
+        }
+    q = aux;
+    aux = aux->next;
+    }
+}
+
+void verification(node *temp)
+{
+    if(temp){
+        printf("Key was found %d\n", temp->data);
+    }
+    else{
+        printf("Key not found\n");
+    } 
+}
+
+void push(node *&pl, int data){
+    //Inserting at first
+    node *nnode = new node(data);
+    nnode->next = pl;
+    pl = nnode;
+}
+
+void insert(node *pl, int data, int pos){
+    //inserting after a given position
+    int i;
+    node *aux = pl;
+    node *nwnode = new node(data);
+    //if you want add new data in the specific 
+    //position given it is need to use "pos-1" instead "pos"
+    for(i = 0; i < pos-1; i++){
+        aux = aux->next;
+    }
+    nwnode->next = aux->next;
+    aux->next = nwnode;  
+}
+
+node *last(node *lp){
+    while (lp->next)
+    {
+     lp = lp->next;   
+    }
+    return lp;
+}
+
+void insertLst(node *lp, int data){
+    node *lst = last(lp);
+    node *aux =  new node(data);
+    lst->next = aux;
+    lst = aux;
+}
+
+void insertaftr(node *lp, int x){ //this function works using an extra pointer too
+    node *aux = lp;
+    while(aux && aux->data < x){
+        aux = aux->next;
+    }
+    if(!aux){               //aux = NULL
+        insertLst(lp, x);
+    }
+    else{
+    node *nnode = new node(aux->data);
+    aux->data = x;
+    nnode->next = aux->next;
+    aux->next = nnode;
+    }
+}
+//Inserting in a sorted list
+void insinor(node *&lp, int x){
+    node *q;
+    node *p = lp;
+    while(p && p->data < x){
+        q = p;
+        p = p->next;
+    }  
+    if(p == lp){
+      push(lp, x);  
+    } 
+    else{
+    node *nnode = new node(x);
+    nnode->next = q->next;
+    q->next = nnode;
+    }
+}
+
 int main()
 {
     node *mylist;
-    int A[] = {7, 9, 5, 3, 4,2};
+    node *temp;
+    int A[] = {3, 5, 7, 9, 12, 24};
     mylist = create(A, 6);
     Rdisplay(mylist);
     printf("\nNumber of nodes is %d\n", Rcount(mylist));
     printf("Sum of data in nodes is %d\n", Rsum(mylist));
+    printf("Maximum number in nodes is %d\n", MaxNmbr(mylist));
+    /*temp = lsearch(mylist, 9);
+    temp = lsearch(mylist, 2);
+    temp = lsearch(mylist, 4);
+    verification(temp);*/
+    /*insert(mylist, 12, 3);
+    push(mylist, 6); //we have added into the front part (also we can name it push)
+    insertLst(mylist, 24);*/
+    //insertaftr(mylist, 2);
+    insinor(mylist,1);
+    Rdisplay(mylist);
+    printf("\nNumber of nodes is %d\n", Rcount(mylist));
+    printf("last element on the list is %d \n", (last(mylist)->data));
     return 0;
 }
